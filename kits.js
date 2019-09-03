1202e78b3f5268ae304a1cabf3a10315b4010d62
SawyaWoo <sawyawoo@qq.com>
2019-09-03 21:21:27 +0800

    update modifyLocalDataById

--- kits.js
+++ kits.js
@@ -2,14 +2,14 @@ var kits = {};
 
 // 1.数字前加0
 // exp: kits.addZero(02) => '2';
-kits.addZero = function(date) {
+kits.addZero = function (date) {
   date = date >= 10 ? date : "0" + date;
   return date;
 };
 
 // 2.获取实时时间
 //exp: 2019-08-20 19:50:16
-kits.getTime = function() {
+kits.getTime = function () {
   var time = new Date();
   var year = time.getFullYear();
   var month = kits.addZero(time.getMonth() + 1);
@@ -22,13 +22,13 @@ kits.getTime = function() {
 
 // 3.获取从n到m的随机数
 //exp: kits.randomNum(1,100) => 1-100随机一个整数
-kits.randomNum = function(n, m) {
+kits.randomNum = function (n, m) {
   return Math.floor(Math.random() * (m - n + 1) + n);
 };
 
 // 4.获取随机ID
 //exp: kits.randomId(1,100) => 1970年至今毫秒数拼接1-100随机一个整数
-kits.randomId = function(n, m) {
+kits.randomId = function (n, m) {
   var time = new Date();
   time = time.getTime();
   var num = kits.randomNum(n, m);
@@ -38,7 +38,7 @@ kits.randomId = function(n, m) {
 
 // 5.获取随机颜色 rgb模式
 //exp: rgb(21,45,164)
-kits.randomRgbColor = function() {
+kits.randomRgbColor = function () {
   var r = kits.randomNum(0, 255);
   var g = kits.randomNum(0, 255);
   var b = kits.randomNum(0, 255);
@@ -48,33 +48,33 @@ kits.randomRgbColor = function() {
 
 // 6.获取随机颜色 hex模式
 //exp: #9cbebe
-kits.randomHexColor = function() {
+kits.randomHexColor = function () {
   var n = ((Math.random() * 0xfffff) | 0).toString(16);
   var result =
     "#" + (n.length !== 6 ? ((Math.random() * 0xf) | 0).toString(16) + n : n);
   return result;
 };
 
-// 7.从localStorage里面根据指定的键(key)获取一个数组 未验证
-kits.getLocalDataArray = function(key) {
+// 7.从localStorage里面根据指定的键(key)获取一个数组 
+kits.getLocalDataArray = function (key) {
   var data = localStorage.getItem(key);
   var arr = JSON.parse(data);
   arr = arr || [];
   return arr;
 };
 
-// 8.将一个数组(arr)以指定的键(key)存储到localStorage里面 未验证
-kits.saveLocalDataArray = function(key, arr) {
+// 8.将一个数组(arr)以指定的键(key)存储到localStorage里面 
+kits.saveLocalDataArray = function (key, arr) {
   var data = JSON.stringify(arr);
   localStorage.setItem(key, data);
 };
 
-// 9.根据对应的id从localStorage中指定键(key)的数组中删除一条数据 未验证
-kits.deleteLocalDataById = function(key, id) {
+// 9.根据对应的id从localStorage中指定键(key)的数组中删除一条数据
+kits.deleteLocalDataById = function (key, id) {
   var data = localStorage.getItem(key);
   var arr = JSON.parse(data);
   arr = arr || [];
-  arr.forEach(function(e, i) {
+  arr.forEach(function (e, i) {
     if (e.id == id) {
       arr.splice(i, 1);
     }
@@ -83,16 +83,16 @@ kits.deleteLocalDataById = function(key, id) {
   localStorage.setItem(key, data);
 };
 
-// 10.根据id修改localStorage里面的指定键(key)的数组数据  未验证
-kits.modifyLocalDataById = function(key, id, datas) {
+// 10.根据id修改localStorage里面的指定键(key)的数组数据  
+kits.modifyLocalDataById = function (key, id, datas) {
   var data = localStorage.getItem(key);
   var arr = JSON.parse(data);
   arr = arr || [];
-  arr.forEach(function(e, i) {
+  arr.forEach(function (e, i) {
     if (e.id == id) {
-      e = datas;
+      arr.splice(i, 1, datas)
     }
   });
   data = JSON.stringify(arr);
   localStorage.setItem(key, data);
-};
+}
